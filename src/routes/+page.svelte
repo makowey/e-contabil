27b915872e2ab1b8740672db22095e0fab815237
Claudiu Macovei <makowey@gmail.com>
2023-09-05 13:18:32 +0300

    initial setup svelte/skeleton

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+
+<div class="container h-full mx-auto flex justify-center items-center">
+	<div class="space-y-10 text-center flex flex-col items-center">
+		<h2 class="h2">PFA Calculator</h2>
+
+
+	</div>
+</div>
+
+<style lang="postcss">
+
+</style>
