f8506511770e737b554176f69221749eefe93e0d
Claudiu Macovei <makowey@gmail.com>
2023-09-07 15:03:33 +0300

    install analytics

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,10 @@
 <script lang="ts">
     import {goto} from "$app/navigation";
+
+    import { dev } from '$app/environment';
+    import { inject } from '@vercel/analytics';
+
+    inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
