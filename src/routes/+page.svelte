5e28741971ba7461d782a39b043c55f5cad6225d
Claudiu Macovei <makowey@gmail.com>
2023-10-06 08:37:13 +0300

    icon...

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,7 @@
 
     import { dev } from '$app/environment';
     import { inject } from '@vercel/analytics';
+    import Icon from "@iconify/svelte";
 
     inject({ mode: dev ? 'development' : 'production' });
 </script>
@@ -11,8 +12,8 @@
 
 <div class="container h-full mx-auto flex justify-center items-center">
     <div class="space-y-10 text-center flex flex-col items-center">
-        <button class="h2 btn btn-md variant-filled-primary cursor-pointer" on:click={() => goto('/pfa')}>
-            PFA Calculator
+        <button class="h2 btn btn-md variant-filled-primary cursor-pointer animate-bounce" on:click={() => goto('/pfa')}>
+            <Icon icon="openmoji:person" /> PFA Calculator
         </button>
     </div>
 </div>
