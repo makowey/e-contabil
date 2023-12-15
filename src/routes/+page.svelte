434c3346e32201e222cba8268c4234234e010e4a
Claudiu Macovei <makowey@gmail.com>
2023-12-15 10:57:56 +0200

    add excel calculator

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
     import {goto} from "$app/navigation";
 
-    import { dev } from '$app/environment';
-    import { inject } from '@vercel/analytics';
+    import {dev} from '$app/environment';
+    import {inject} from '@vercel/analytics';
     import Icon from "@iconify/svelte";
 
-    inject({ mode: dev ? 'development' : 'production' });
+    inject({mode: dev ? 'development' : 'production'});
 </script>
 
 <svelte:head>
@@ -14,8 +14,15 @@
 
 <div class="container h-full mx-auto flex justify-center items-center">
     <div class="space-y-10 text-center flex flex-col items-center">
-        <button class="h2 btn btn-md variant-filled-primary cursor-pointer animate-bounce" on:click={() => goto('/pfa')}>
-            <Icon icon="openmoji:person" /> PFA Calculator
+        <button class="h2 btn btn-md variant-filled-primary cursor-pointer animate-bounce"
+                on:click={() => goto('/pfa')}>
+            <Icon icon="openmoji:person"/>
+            PFA Calculator
+        </button>
+        <button class="h2 btn btn-md variant-filled-primary cursor-pointer animate-bounce"
+                on:click={() => goto('/doc/pfa-vs-cim-vs-srl.xlsx')}>
+            <Icon icon="openmoji:abacus"/>
+            Excel pfa/cim/srl-simulator
         </button>
     </div>
 </div>
